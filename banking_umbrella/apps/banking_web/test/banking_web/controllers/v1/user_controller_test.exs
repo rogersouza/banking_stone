757d86078e47cf332d22c3d5e82d866b6939b45f
Roger Souza <rogersec42@gmail.com>
2019-08-25 11:37:45 -0300

    Move all related tests to inside the describe block

--- banking_umbrella/apps/banking_web/test/banking_web/controllers/v1/user_controller_test.exs
+++ banking_umbrella/apps/banking_web/test/banking_web/controllers/v1/user_controller_test.exs
@@ -21,18 +21,18 @@ defmodule BankingWeb.V1.UserControllerTest do
       assert %{"email" => ^expected_email} = response["account"]
       assert %{"name" => ^expected_name} = response
     end
-  end
 
-  test "returns 201 on successfuly created user", %{conn: conn} do
-    conn = post(conn, api_v1_user_path(conn, :create), @user_fixture)
-    assert json_response(conn, 201)
-  end
+    test "returns 201 on successfuly created user", %{conn: conn} do
+      conn = post(conn, api_v1_user_path(conn, :create), @user_fixture)
+      assert json_response(conn, 201)
+    end
 
-  test "returns 400 in case of validation errors", %{conn: conn} do
-    account = %{"email" => "invalid_email.com", "password" => "password123"}
-    user_fixture = %{@user_fixture | "account" => account}
-    conn = post(conn, api_v1_user_path(conn, :create), user_fixture)
-    
-    assert json_response(conn, 400)
+    test "returns 400 in case of validation errors", %{conn: conn} do
+      account = %{"email" => "invalid_email.com", "password" => "password123"}
+      user_fixture = %{@user_fixture | "account" => account}
+      conn = post(conn, api_v1_user_path(conn, :create), user_fixture)
+
+      assert json_response(conn, 400)
+    end
   end
 end
