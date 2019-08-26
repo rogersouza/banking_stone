7a5f04a76f0b0ce0ad1f8b2e0bd772f13c9c4009
Roger Souza <rogersec42@gmail.com>
2019-08-26 19:41:18 -0300

    Ensure that all endpoints requires the JWT
    Except for the POST /api/v1/login and POST /api/v1/user

--- banking_umbrella/apps/banking_web/test/banking_web/controllers/v1/customer_controller_test.exs
+++ banking_umbrella/apps/banking_web/test/banking_web/controllers/v1/customer_controller_test.exs
@@ -7,7 +7,23 @@ defmodule BankingWeb.V1.CustomerControllerTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Banking.Repo)
   end
 
+  @email "test@mail.com"
+  @password "password123456
+  "
+  @user_fixture %{
+    "account" => %{"email" => @email, "password" => @password},
+    "name" => "Tester"
+  }
+
   describe "create/2" do
+    setup %{conn: conn} do
+      {:ok, user} = Authentication.register(@user_fixture)
+      {:ok, token} = Authentication.sign_in(%{"email" => @email, "password" => @password})
+
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+      %{conn: conn}
+    end
+
     test "returns 201 when users is created", %{conn: conn} do
       customer = %{"email" => "jane@mail.com", "name" => "Jane Doe"}
       conn = post(conn, api_v1_customer_path(conn, :create), customer)
@@ -29,7 +45,7 @@ defmodule BankingWeb.V1.CustomerControllerTest do
     test "returns 409 if there's already a customer with this email", %{conn: conn} do
       customer = %{"email" => "jane@mail.com", "name" => "Jane Doe"}
       Banking.create_customer(customer)
-      
+
       conn = post(conn, api_v1_customer_path(conn, :create), customer)
       assert json_response(conn, 409)
     end
