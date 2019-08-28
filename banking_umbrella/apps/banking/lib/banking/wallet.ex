74e7a94895f34fc8a4a2a965b62518a62448e007
Roger Souza <rogersec42@gmail.com>
2019-08-28 09:19:27 -0300

    Add constraint to wallet's balance so it is always a positive number

--- banking_umbrella/apps/banking/lib/banking/wallet.ex
+++ banking_umbrella/apps/banking/lib/banking/wallet.ex
@@ -1,6 +1,12 @@
 defmodule Banking.Wallet do
-  @moduledoc false
-  
+  @moduledoc """
+  A customer's wallet. It holds the current balance of a customer.
+
+  After a transaction is inserted, the balance should be updated.
+
+  A WALLET SHOULD ALWAYS HAVE A POSITIVE BALANCE
+  """
+
   use Ecto.Schema
 
   import Ecto.Changeset
@@ -9,13 +15,17 @@ defmodule Banking.Wallet do
   @optional_fields []
 
   schema "wallets" do
-    belongs_to :customer, Banking.Customer
-    field :balance, Money.Ecto.Amount.Type
+    belongs_to(:customer, Banking.Customer)
+    field(:balance, Money.Ecto.Amount.Type)
   end
 
   def changeset(wallet, params) do
     wallet
     |> cast(params, @optional_fields ++ @required_fields)
     |> validate_required(@required_fields)
+    |> check_constraint(:balance,
+      name: :balance_must_be_positive,
+      message: "balance must be positive"
+    )
   end
 end
